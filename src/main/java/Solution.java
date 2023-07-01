de1a0ec6a5630d683a97b47d725fee31e9a1852d
alek <wait82985@gmail.com>
2023-07-02 00:01:08 +0600

    problem solved. Backtracking

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,32 @@
+import java.util.Arrays;
+
 public class Solution {
     public int distributeCookies(int[] cookies, int k) {
-        //TODO
-        return 0;
+
+        return dfs(cookies, new int[k], 0);
+    }
+
+    private int dfs(int[] cookies, int[] children, int i) {
+        if (i == cookies.length) {
+
+            int max = Integer.MIN_VALUE;
+            for (int j = 0; j < children.length; j++) {
+                if (children[j] > max) {
+                    max = children[j];
+                }
+            }
+
+            return max;
+        }
+
+        int result = Integer.MAX_VALUE;
+
+        for (int j = 0; j < children.length; j++) {
+            children[j] += cookies[i];
+            result = Math.min(result, dfs(cookies, children, i + 1));
+            children[j] -= cookies[i];
+        }
+
+        return result;
     }
 }
